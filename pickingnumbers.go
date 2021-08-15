192c2f63d7b076d9b573210680a0e7a173e6735d
r <rhcampbell79@gmail.com>
2021-08-14 19:10:56 -0500

    Added func findLrg

--- pickingnumbers.go
+++ pickingnumbers.go
@@ -5,8 +5,8 @@ import(
 )
 
 func main() {
-	a := []int32{5,4,6,5,3,3,1,2,3,9,3,4,5,5,6}
-	//{1,1,1,2,2,2,4,4,4,4,4}
+	// a := []int32{5,4,6,5,3,3,1,2,3,9,3,4,5,5,6}
+	a := []int32{1,2,3,4,5,6,7}
 
 	fmt.Println(pickingNumbers(a))
 }
@@ -14,23 +14,25 @@ func main() {
 func pickingNumbers(a []int32) int32 {
 	matches := repeatNumbers(a)
 	
-	var lrg int32 = 0
+	var lrg int32 = findLrg(matches) + 1
 	var add int32 = 0
 	
 	for i := 0; i < len(matches); i++ {
-		newI := fmt.Sprintf("%d", i)
-		newIPl := fmt.Sprintf("%d", i+1)
-		sub := matches["p"+newI][0] - matches["p"+newIPl][0]
-		if sub < 0  {
-			sub = sub * -1
-		}
+		for j := i+1; j < len(matches); j++ {
+			newI := fmt.Sprintf("%d", i)
+			newIPl := fmt.Sprintf("%d", j)
+			sub := matches["p"+newI][0] - matches["p"+newIPl][0]
+			if sub < 0  {
+				sub = sub * -1
+			}
 
-		if sub <= 1 {
-			add = matches["p"+newI][1] + matches["p"+newIPl][1]
-		}
+			if sub <= 1 {
+				add = matches["p"+newI][1] + matches["p"+newIPl][1]
+			}
 
-		if add > lrg {
-			lrg = add
+			if add > lrg {
+				lrg = add
+			}
 		}
 	}
 
@@ -47,21 +49,10 @@ func repeatNumbers(a []int32) map[string][]int32 {
 	l := len(a)
 	var count int32
 	var keyCount int32 = 0
-	//match := false
 	var repeater int32
 	repeatMap := make(map[string][]int32)
-	//repeatArr := []int32{}
 	flag := false
 
-	// for i := 0; i < l; i++ {
-	// 	for j := i+1; j < l; j++ {
-	// 		if a[i] == a[j] {
-	// 			count++
-	// 			break
-	// 		}
-	// 	}
-	// }
-	//a := []int32{5,4,6,5,3,3,1,2,3,9,3,4,5,5,6}
 	for i := 0; i < l; i++ {
 		repeatArr := []int32{}
 		for _, x := range repeatMap {
@@ -96,11 +87,18 @@ func repeatNumbers(a []int32) map[string][]int32 {
 		repeater = 0
 	}
 
-	// if count > 0 {
-	// 	match = true
-	// } else {
-	// 	match = false
-	// }
-
 	return repeatMap
+}
+
+func findLrg(m map[string][]int32) int32 {
+	var lrg int32 = 0
+
+	for i := 0; i < len(m); i++ {
+		newI := fmt.Sprintf("%d", i)
+		if m["p"+newI][1] > lrg {
+			lrg = m["p"+newI][1]
+		}
+	}
+
+	return lrg
 }
